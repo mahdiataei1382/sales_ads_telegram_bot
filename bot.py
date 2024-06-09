de9808db72ee8e78ac1d244eef2b1cd00e290a1e
mahdiataei1382 <142623925+mahdiataei1382@users.noreply.github.com>
2024-06-10 03:15:15 +0330

    bot.py

--- bot.py
+++ bot.py
@@ -1,169 +1,207 @@
-import logging
-from uuid import uuid4
-from telegram import Update , InlineQueryResultAudio
-from telegram.ext import Application, CommandHandler, ContextTypes , MessageHandler , ConversationHandler , filters , InlineQueryHandler
-import json
-TOKEN = "7181380651:AAHuojuYyXPoZCcwIDb0Q6kpgZrj7rd883A"
+from typing import Final
 
-logging.basicConfig(format="%(asctime)s - %(name)s - % (levelname)s - %(message)s", level=logging.INFO)
-logging.getLogger("httpx").setLevel(logging.INFO)
-logger = logging.getLogger(__name__)
+from telegram import (
+    Update,
+    InlineQueryResultPhoto,
+)
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    ConversationHandler,
+    filters,
+    MessageHandler,
+    InlineQueryHandler,
+)
 
-admins = [408345002]
-AUdio = 0
-SONGER = 1
-NAME = 2
-file = open('music_data.json' , 'r')
-musics = json.load(file)
-file.close()
+from mongo_client import AdsMongoClient
 
-async def add_aduio_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_user.id in admins:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="فایل اهنگ رو ارسال کن",
-        )
-        return AUdio
+BOT_TOKEN: Final = "6559276107:AAGYkl2Mqc6LC3uLVY4UQa4h6RVcImOvjxs"
 
-    else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="شما دسترسی ندارید",
-        )
-        return add_music.END
+CATEGORY, PHOTO, DESCRIPTION = range(3)
+# db connection
+db_client = AdsMongoClient("localhost", 27017)
+# add your user ids here, you can use @userinfobot to get your user id
+# DO NOT REMOVE EXISTING IDs
+dev_ids = [92129627, 987654321 , 408345002]
 
 
-async def send_audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    file_id = update.effective_message.audio.file_id
-    context.user_data['file_id'] = file_id
+async def start_command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="اسم خواننده رو بفرست",
+        text="سلام، من ربات ثبت آگهی هستم. برای قبت آگهی جدید از دستور /add_advertising استفاده کنید.",
+        reply_to_message_id=update.effective_message.id,
     )
-    return SONGER
 
 
-async def send_songer(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    songer = update.effective_message.text
-    context.user_data['songer'] = songer
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="اسم اهنگ رو بفرست",
-    )
-    return NAME
-
-
-async def send_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    name = update.effective_message.text
-    music = {'file_id': context.user_data['file_id'], 'songer': context.user_data['songer'], 'name': name}
-    musics.append(music)
-    file = open('music_data.json','w')
-    json.dump(musics , file)
-    file.close()
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text='اهنگ اضافه شد',
-    )
-    return add_music.END
-
-
-async def cancel_command_handler(
-        update: Update, context: ContextTypes.DEFAULT_TYPE
+async def add_category_command_handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
 ):
-    text = "اضافه کردن اهنگ کنسل شد"
+    if update.effective_user.id in dev_ids :
+        category = ""
+        for word in context.args:
+            category = category + word + " "
+        category = category[:-1]
+        db_client.add_category(category)
+        text  = f"دسته بندی {category} با موفقیت اضافه شد."
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=text,
+            reply_to_message_id=update.effective_message.id
+        )
+
+    else :
+        text = "شما اجازه دسترسی به این دستور را ندارید."
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=text,
+            reply_to_message_id=update.effective_message.id
+        )
+
+
+async def add_advertising_command_handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    categories = db_client.get_categories()
+    text = "لطفا از بین دسته بندی های زیر یکی را انتخاب کنید:\n" + "\n".join(categories)
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=text,
         reply_to_message_id=update.effective_message.id
     )
-    return add_music.END
+    return CATEGORY
 
 
-async def serach_music(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    name = update.effective_message.text
-    for music in musics:
-        if music['name'] == name:
-            await context.bot.send_audio(
-                chat_id=update.effective_chat.id,
-                audio=music['file_id']
-            )
-            return
+async def choice_category_message_handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    context.user_data["category"] = update.effective_message.text
+    text = "لطفا عکس آگهی خود را ارسال کنید."
+    await context.bot.send_message(
+        chat_id= update.effective_chat.id ,
+        text = text ,
+        reply_to_message_id= update.effective_message.id
+    )
+    return PHOTO
+
+
+async def photo_message_handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    context.user_data["photo_url"] = update.effective_message.photo[-1].file_id
+    text = "لطفا توضیحات آگهی خود را وارد کنید. در توضیحات می توانید اطلاعاتی مانند قیمت، شماره تماس و ... را وارد کنید."
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="not found"
+        text=text,
+        reply_to_message_id=update.effective_message.id
+    )
+    return DESCRIPTION
+
+async def description_message_handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    description = update.effective_message.text
+    user_id = int(update.effective_user.id)
+    photo_url = str(context.user_data["photo_url"])
+    category = str(context.user_data["category"])
+    db_client.add_advertising(user_id=user_id , photo_url=str(photo_url) , category=category , description=description)
+    text = "آگهی شما با موفقیت ثبت شد."
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text,
+        reply_to_message_id=update.effective_message.id
+    )
+    return ConversationHandler.END
+
+
+async def cancel_command_handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    text = "عملیات ثبت آگهی لغو شد. برای ثبت آگهی جدید از دستور /add_category استفاده کنید."
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text,
+        reply_to_message_id=update.effective_message.id
+    )
+    return ConversationHandler.END
+
+async def my_ads_command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    ads = db_client.get_user_ads(update.effective_user.id)
+    if ads==[]:
+       text =  "شما هیچ آگهی ثبت نکرده‌اید."
+       await context.bot.send_message(
+           chat_id=update.effective_chat.id ,
+           text = text ,
+           reply_to_message_id=update.effective_message.id
+       )
+    else :
+        for ad in ads :
+            await context.bot.send_photo(
+                chat_id=update.effective_chat.id,
+                photo=ad["photo_url"],
+                caption=ad["description"] + f"\n\n" + "برای حذف آگهی از دستور زیر استفاده کنید." + "\n\n" + f"/delete_ad {ad['id']}",
+                reply_to_message_id=update.effective_message.id,
+            )
+
+async def delete_ad_command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    ad_id = context.args[0]
+    db_client.delete_advertising(update.effective_user.id , ad_id)
+    text = "آگهی با موفقیت حذف شد."
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text,
+        reply_to_message_id=update.effective_message.id
     )
 
-
-async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=("""به بات اهنگهای بانو هایده خوش آمدید
-دانلود موزیک ها /music
-دانلود موزیک ویدیو /musicvideo
-را کلیک کنید"""))
-
-
-async def music_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="""لیست موزیک ها:
-اهنگ شب عشق > /shabeshg
-اهنگ اشاره > /eshare
-اهنگ وای به حالش > /vaybehalesh
-اهنگ ای زندگی سلام > /eyzendegisalam
-اهنگ پریشون > /parishoon
-اهنگ سال> /sal
-اهنگ بهانه > /bahaneh
-اهنگ عالم یکرنگی > /alamyekrangi
-اهنگ راوی > /ravi
-اهنگ گل سنگ > /golsang""")
-
-
-async def musicvideo_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="""لیست موزیک ویدیو ها:
-اهنگ شب عشق > /shabeshgv
-اهنگ اشاره > /esharev
-اهنگ وای به حالش > /vaybehaleshv
-اهنگ ای زندگی سلام > /eyzendegisalamv
-اهنگ پریشون > /parishonv
-اهنگ سال> /salv
-اهنگ شهر آشوب > /shahreashobv
-اهنگ عالم یکرنگی > /alamyekrangiv
-اهنگ راوی > /ravi
-اهنگ گل سنگ > /golsangv""")
-
-async def inline_query_handler( update : Update , context: ContextTypes.DEFAULT_TYPE) :
-    query = update.inline_query.query
+async def search_ads_by_category_inline_query(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+):
+    category = update.inline_query.query
+    ads = db_client.get_ads_by_category(category=category)
     results = []
-    if not query:
-        for music in musics:
-            result = InlineQueryResultAudio(
-                id=str(uuid4()),
-                audio_url=music['file_id'],
-                title=music['name']
-            )
-            results.append(result)
+    for ad in ads :
+        result = InlineQueryResultPhoto(
+            id = ad["id"] ,
+            title=ad["description"] ,
+            photo_url=ad["photo_url"] ,
+            thumbnail_url=ad["photo_url"],
+            caption=ad["description"],
+        )
+        results.append(result)
 
-    else :
-        for music in musics :
-            if music['name'] == query :
-                result = InlineQueryResultAudio(
-                    id= str(uuid4()) ,
-                    audio_url= music['file_id'] ,
-                    title= music['name']
-                )
-                results.append(result)
-    await update.inline_query.answer(results)
+    await context.bot.answer_inline_query(update.inline_query.id , results)
 
 if __name__ == "__main__":
-    bot = Application.builder().token(TOKEN).build()
-    add_music = ConversationHandler(entry_points=[CommandHandler("add_audio", add_aduio_handler)], states={
-        AUdio: [MessageHandler(filters.AUDIO, send_audio)],
-        SONGER: [MessageHandler(filters.TEXT, send_songer)],
-        NAME: [MessageHandler(filters.TEXT, send_name)], },
-                                    fallbacks=[CommandHandler("cancel", cancel_command_handler), ], )
-    bot.add_handler(add_music)
-    bot.add_handler(InlineQueryHandler(inline_query_handler))
-    bot.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, serach_music))
-    bot.add_handler(CommandHandler('start', start_handler))
-    bot.add_handler(CommandHandler('music', music_handler))
-    bot.add_handler(CommandHandler('musicvideo', musicvideo_handler))
-    bot.run_polling()
-
-
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start_command_handler))
+    app.add_handler(CommandHandler("add_category", add_category_command_handler))
+    app.add_handler(
+        ConversationHandler(
+            entry_points=[
+                CommandHandler("add_advertising", add_advertising_command_handler)
+            ],
+            states={
+                CATEGORY: [
+                    MessageHandler(
+                        filters.TEXT & ~filters.COMMAND, choice_category_message_handler
+                    )
+                ],
+                PHOTO: [
+                    MessageHandler(filters.PHOTO, photo_message_handler),
+                ],
+                DESCRIPTION: [
+                    MessageHandler(
+                        filters.TEXT & ~filters.COMMAND, description_message_handler
+                    )
+                ],
+            },
+            fallbacks=[
+                CommandHandler("cancel", cancel_command_handler),
+            ],
+            allow_reentry=True,
+        )
+    )
+    app.add_handler(CommandHandler("delete_ad" , delete_ad_command_handler))
+    app.add_handler(CommandHandler("my_ads", my_ads_command_handler))
+    app.add_handler(InlineQueryHandler(search_ads_by_category_inline_query))
+    app.run_polling()
